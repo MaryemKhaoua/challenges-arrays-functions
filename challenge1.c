70a43de8068c96654b7bcdfdaecbd77037c45277
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-09-18 18:21:29 +0100

    first challenge

---
+++ challenge1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int i;
+  
+  for (i = 0; i < 10; i++) 
+  {
+    printf("%d, ", arr[i]);
+  }
+ 
+  return 0;
+}
